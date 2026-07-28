0a8f9ffee3952ac47bf26939e7e7805503d388f6
Molhamalfa <156117559+Molhamalfa@users.noreply.github.com>
2026-07-28 07:32:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 export default async function handler(req, res) {
-  // Forward the request to Google API using the exact same path
-  const targetUrl = `https://generativelanguage.googleapis.com${req.url}`;
+  const { path } = req.query;
+  const urlPath = Array.isArray(path) ? path.join('/') : path;
+  
+  const searchParams = new URLSearchParams(req.query);
+  searchParams.delete('path');
+  const queryString = searchParams.toString();
+  
+  const targetUrl = `https://generativelanguage.googleapis.com/${urlPath}${queryString ? '?' + queryString : ''}`;
   
   try {
     const response = await fetch(targetUrl, {
